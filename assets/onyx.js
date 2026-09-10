ab76010eaeed901caebdb35fb323f4665246c7c8
lancherosthomas-hub <lancheros.thomas@student.oaklandcc.edu>
2026-09-10 12:49:44 -0400

    hglglh

--- assets/onyx.js
+++ assets/onyx.js
@@ -92,6 +92,7 @@ window.Onyx = (function () {
     { href: '/pipeline/', label: 'Pipeline' },
     { href: '/bad-reviewed/', label: 'Bad Reviewed Locations' },
     { href: '/calls/', label: 'Call list' },
+    { href: '/dial-mobile/', label: 'Dial mobile' },
     { href: '/email/', label: 'Email' },
     { href: '/admin/', label: 'Admin', adminOnly: true }
   ];
